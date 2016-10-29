c9d909ef015989b13c19ccfe7fcb146fcd55c275
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-10-29 16:24:52 -0500

    preprocessing done

--- pkotha6_sanilk2.py
+++ pkotha6_sanilk2.py
@@ -1,5 +1,4 @@
 import openpyxl as px
-import numpy as np
 import csv
 import re
 
@@ -8,9 +7,9 @@ sheet = workbook.get_sheet_by_name(name='Obama')
 
 max_row = sheet.max_row
 
-end = 'E'+ str(max_row)
+end = 'E' + str(max_row)
 csv_file = open('preprocessed.csv', 'w', newline='')
-wr = csv.writer(csv_file, delimiter=',', quoting = csv.QUOTE_ALL)
+wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
 
 stopwords = ['a', 'an', 'another', 'any', 'certain', 'each', 'every', 'her', 'his', 'i', 'its', 'its', 'my', '"no',
              'our', 'some', 'that', 'the', 'their', 'this', 'and', 'but', 'or', 'yet', 'for', 'nor', 'so', 'as',
@@ -22,11 +21,14 @@ stopwords = ['a', 'an', 'another', 'any', 'certain', 'each', 'every', 'her', 'hi
 
 def preprocess(cur_tweet):
     cur_tweet = cur_tweet.lower().strip()
+    cur_tweet = cur_tweet.replace("\"", "")
     cur_tweet = re.sub(r'@[^\s]+', '', cur_tweet).strip()
-    cur_tweet = re.sub(r'\#+[\w_]+[\w\'_\-]*[\w_]+', '', cur_tweet).strip()
+    cur_tweet = re.sub(r'#([^\s]+)', r'\1', cur_tweet).strip()
     cur_tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', '', cur_tweet).strip()
     cur_tweet = re.sub(r'<[^>]+>', '', cur_tweet).strip()
     cur_tweet = re.sub(r'[\s]+', ' ', cur_tweet).strip()
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    cur_tweet =  pattern.sub(r"\1\1", cur_tweet)
     processed_tweet = []
     words = re.findall(r'[\w]+', cur_tweet)
     for word in words:
@@ -40,13 +42,14 @@ def preprocess(cur_tweet):
 
 for row in sheet['D3':end]:
     if row[1].value in [1,-1,0]:
-        preproceesed_tweet = preprocess(str(row[1].value))
-        if preproceesed_tweet.strip():
-            tweets = [row[0].value, preproceesed_tweet]
+        preprocessed_tweet = preprocess(str(row[0].value))
+        if preprocessed_tweet.strip():
+            tweets = [str(preprocessed_tweet), row[1].value]
             wr.writerow(tweets)
 
 
-csv_file.close()
-
-
-
+print(preprocess)
+# def test(tweet):
+#     print(preprocess(tweet))
+#
+# test("@sds #happy https://ssfdsf.com obama's <a> he's here hunggrryy happy huuuuuuuuuungry </a>")
