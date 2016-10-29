e414c42fd7e3d5ae744dfcc80a3471b2cd9cf21e
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-10-29 17:17:02 -0500

    Added stop words

--- pkotha6_sanilk2.py
+++ pkotha6_sanilk2.py
@@ -11,13 +11,9 @@ end = 'E' + str(max_row)
 csv_file = open('preprocessed.csv', 'w', newline='')
 wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
 
-stopWords = ['a', 'an', 'another', 'any', 'certain', 'each', 'every', 'her', 'his', 'i', 'its', 'its', 'my', '"no',
-             'our', 'some', 'that', 'the', 'their', 'this', 'and', 'but', 'or', 'yet', 'for', 'nor', 'so', 'as',
-             'aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'around', 'at', 'before', 'behind',
-             'below', 'beneath', 'beside', 'between', 'beyond', 'but', 'by', 'down', 'during', 'except', 'following',
-             'for', 'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'next', 'of', 'off', 'on',
-             'onto', 'opposite', 'out', 'outside', 'over', 'past', 'plus', 'round', 'since', 'than', 'through', 'to',
-             'toward', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'with', 'without']
+stopWords = []
+with open('StopWords.txt') as stop_file:
+    stopWords = [word for word in stop_file]
 
 def preprocess(cur_tweet):
     cur_tweet = cur_tweet.lower().strip()
@@ -28,7 +24,7 @@ def preprocess(cur_tweet):
     cur_tweet = re.sub(r'<[^>]+>', '', cur_tweet).strip()
     cur_tweet = re.sub(r'[\s]+', ' ', cur_tweet).strip()
     pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
-    cur_tweet =  pattern.sub(r"\1\1", cur_tweet)
+    cur_tweet = pattern.sub(r"\1\1", cur_tweet)
     processed_tweet = []
     words = re.findall(r'[\w]+', cur_tweet)
     for word in words:
@@ -43,9 +39,9 @@ def preprocess(cur_tweet):
 
 for row in sheet['D3':end]:
     if row[1].value in [1,-1,0]:
-        preproceesed_tweet = preprocess(str(row[0].value))
-        if preproceesed_tweet.strip():
-            tweets = [preproceesed_tweet, row[1].value]
+        prepocessed_tweet = preprocess(str(row[0].value))
+        if prepocessed_tweet.strip():
+            tweets = [prepocessed_tweet, row[1].value]
             wr.writerow(tweets)
 
 
@@ -68,13 +64,13 @@ tweets = []
 inpTweets = csv.reader(open('preprocessed.csv', 'rt'), delimiter=',', quotechar='|')
 i = 0
 for row in inpTweets:
-    i+=1
+    i += 1
     sentiment = row[1]
     tweet = row[0]
     featureVector = getFeatureVector(tweet)
     tweets.append((featureVector, sentiment))
-    if(i==10):
+    if i == 10:
         break
 
-print (tweets)
+print(tweets)
 
