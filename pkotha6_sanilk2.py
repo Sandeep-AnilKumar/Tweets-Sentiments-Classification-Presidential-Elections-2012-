36077b71c4dc7b2e2f70fd4c5278ed211f87c543
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-10-30 16:52:04 -0500

    classified some tweets

--- pkotha6_sanilk2.py
+++ pkotha6_sanilk2.py
@@ -1,7 +1,6 @@
 import nltk
 import csv
-import preprocessor
-
+from sklearn.model_selection import KFold
 
 def extract_features(tweet):
     tweet_words = set(tweet)
@@ -13,36 +12,42 @@ def extract_features(tweet):
 tweets = []
 inpTweets = csv.reader(open('preprocessed.csv', 'rt'), delimiter=',')
 featureList = []
-i = 0
+
 for row in inpTweets:
-    i += 1
     sentiment = row[1]
     tweet = row[0]
     featureVector = [word.lower().strip() for word in tweet.split()]
     featureList.extend(featureVector)
     tweets.append((featureVector, sentiment))
-    if i == 500:
-        break
 
-print("after for loop")
 featureList = list(set(featureList))
-print("after for loop")
+kf = KFold(n_splits=10)
+fold_number = 1
+# tweets = tweets[:1000]
+total_accuracy = 0
+for train, test in kf.split(tweets):
+    # Training
+    correct_count = 0
+    training_tweets = [tweets[t] for t in train]
+    training_set = nltk.classify.util.apply_features(extract_features, training_tweets)
+    NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
+    # Testing
+    for t in test:
+        tweet_test = tweets[t][0]
+        actual_class = tweets[t][1]
+        classified_class = NBClassifier.classify(extract_features(tweet_test))
+        print("Tweet : ", tweet_test)
+        print("Actual class : ", actual_class)
+        print("Classified class : ", classified_class)
+        print("######################################################")
+        if str(actual_class) == str(classified_class):
+            correct_count += 1
+    cur_fold_accuracy = correct_count / len(test)
+    print("Accuracy for fold :", fold_number," -> ", cur_fold_accuracy)
+    fold_number += 1
+    print("######################################################")
+    total_accuracy += cur_fold_accuracy
 
-#Training the classifier
-training_set = nltk.classify.util.apply_features(extract_features, tweets)
-print("traning")
-NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
-print("classifier building")
+print("Total Classifier accuracy :", total_accuracy / 10)
 
-# Test the classifier
-testTweet = "obama debates ass cracker pataki"
-processedTestTweet = preprocessor.preprocess(testTweet)
-print(NBClassifier.classify(extract_features([word.lower().strip() for word in processedTestTweet.split()])))
 
-testTweet = "love how polls are reported news when obama was lead they were saying how important they were now"
-processedTestTweet = preprocessor.preprocess(testTweet)
-print(NBClassifier.classify(extract_features([word.lower().strip() for word in processedTestTweet.split()])))
-
-testTweet = "smart women know when obama said policies would not cause energy prices will not skyrocket he meant it"
-processedTestTweet = preprocessor.preprocess(testTweet)
-print(NBClassifier.classify(extract_features([word.lower().strip() for word in processedTestTweet.split()])))
