0759862d2d8710bc63adce535852c63df6f9bf18
Sandeep Kumar Anil Kumar <sandeepkanil@gmail.com>
2016-11-28 21:25:06 -0600

    ran grid search for best parameters and chose it

--- demo.py
+++ demo.py
@@ -1,98 +1,74 @@
 import csv
-from sklearn.model_selection import KFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn import linear_model
 from sklearn import svm
 from sklearn.utils import shuffle
-from nltk.stem import PorterStemmer
 from sklearn import metrics
 from sklearn.naive_bayes import BernoulliNB
-import time
 
-tweets = list()
-input_tweets = csv.reader(open('preprocessed.csv', 'rt'), delimiter=',')
-tweets_text = list()
-unshuffled_tweets = list()
+def training_and_testing(candidate):
+    input_tweets = csv.reader(open(candidate + '_training.csv', 'rt'), delimiter=',')
+    unshuffled_tweets = list()
 
-for row in input_tweets:
-    unshuffled_tweets.append(row)
+    for row in input_tweets:
+        unshuffled_tweets.append(row)
 
-unshuffled_tweets = shuffle(unshuffled_tweets, random_state=0)
+    shuffled_tweets = shuffle(unshuffled_tweets, random_state=0)
 
-tweets_text = list()
-tweet_labels = list()
+    training_tweets_text = list()
+    train_labels = list()
 
-for row in unshuffled_tweets:
-    sentiment = row[1]
-    tweet = row[0]
-    tweets_text.append(tweet)
-    tweet_labels.append(sentiment)
-    feature_vector = [word.lower().strip() for word in tweet.split()]
-    tweets.append((feature_vector, sentiment))
+    for row in shuffled_tweets:
+        sentiment = row[1]
+        tweet = row[0]
+        training_tweets_text.append(tweet)
+        train_labels.append(sentiment)
 
-kf = KFold(n_splits=10)
-fold_number = 1
-total_accuracy = 0
+    # reading the test tweets from testFile
+    test_tweets = csv.reader(open(candidate + '_testing.csv', 'rt'), delimiter=',')
+    test_tweets_text = list()
+    test_labels = list()
+    for row in test_tweets:
+        tweet = row[0]
+        test_tweets_text.append(tweet)
+        test_labels.append(row[1])
 
-#reading the test tweets from testFile
-# test_tweets = csv.reader(open('preprocessed.csv', 'rt'), delimiter=',')
-# testTweetsText = list()
-# testTweetLabels = list()
-# for row in test_tweets:
-#     tweet = row[0]
-#     testTweetsText.append(tweet)
-#     testTweetLabels.append(row[1])
-#     feature_vector = [word.lower().strip() for word in tweet.split()]
-
-voting_fold_accuracies = list()
-fold_number = 1
-for train, test in kf.split(tweets):
-    # Training
-    start_time = time.time()
-    training_tweets = [tweets[t] for t in train]
-
-    training_tweets_text = [tweets_text[k] for k in train]
-    test_tweets_text = [tweets_text[j] for j in test]
-    train_labels = [tweets[t][1] for t in train]
-    test_labels = [tweets[t][1] for t in test]
-
-    tfidf_transformer = TfidfVectorizer()
-    train_tfidf_vector = tfidf_transformer.fit_transform(training_tweets_text)
-    X_new_tfidf = tfidf_transformer.transform(test_tweets_text)
+    tf_idf_vectorizer = TfidfVectorizer()
+    train_tf_idf_vector = tf_idf_vectorizer.fit_transform(training_tweets_text)
+    test_tweets_vector = tf_idf_vectorizer.transform(test_tweets_text)
 
     #SVM
-    svm_clf = svm.SVC(kernel='linear', C=1.1, gamma=1.1)
+    svm_clf = svm.SVC(kernel='rbf', C=1.1, gamma=1.1)
 
     #SGDC classifier
     sgd_clf = linear_model.SGDClassifier()
 
     #knn classifier
-    knn_clf = KNeighborsClassifier(n_neighbors=5)
+    knn_clf = KNeighborsClassifier(n_neighbors=7)
 
-    #logistic reression
+    #logistic regression
     lr_clf = linear_model.LogisticRegression(C=1e5)
 
     #Naive Bayes
     nb_clf = BernoulliNB(alpha=.01)
 
-    voting_clf = VotingClassifier(estimators=[('lr', lr_clf), ('nb', nb_clf), ('svm', svm_clf), ('sgd', sgd_clf), ('knn', knn_clf)], voting='hard')
-    voting_clf = voting_clf.fit(train_tfidf_vector, train_labels)
-    voting_prediction = voting_clf.predict(X_new_tfidf)
+    voting_clf = VotingClassifier(estimators=[('nb', nb_clf), ('svm', svm_clf), ('sgd', sgd_clf)], voting='hard')
+    voting_clf = voting_clf.fit(train_tf_idf_vector, train_labels)
+    voting_prediction = voting_clf.predict(test_tweets_vector)
 
     voting_accuracy = metrics.accuracy_score(test_labels, voting_prediction)
-    print("Fold accuracy for", fold_number, "is :=", voting_accuracy * 100, "%")
-    voting_fold_accuracies.append(voting_accuracy)
-    print("total time for fold", fold_number, "is", time.time() - start_time)
-    fold_number += 1
+    confusion_metrics = metrics.confusion_matrix(test_labels, voting_prediction)
 
+    print(confusion_metrics)
+    print("Total accuracy for", candidate, "is", voting_accuracy * 100, "%")
 
-aggregated_accuracy = 0;
+    print("Precision score for LR for Testing Data", metrics.precision_score(test_labels, voting_prediction, average=None))
+    print("Recall score for LR for Testing Data", metrics.recall_score(test_labels, voting_prediction, average=None))
+    print("F1 score for LR for Testing Data", metrics.f1_score(test_labels, voting_prediction, average=None))
+    print("finish\n-----------------------------\n\n")
 
-for accuracy in voting_fold_accuracies:
-    aggregated_accuracy += accuracy
-
-print("Total accuracy is", aggregated_accuracy * 10, "%")
-print("finish")
+training_and_testing('Obama')
+training_and_testing('Romney')
 
--- preprocessor.py
+++ preprocessor.py
@@ -1,15 +1,13 @@
 import openpyxl as px
 import csv
 import re
+from nltk.stem import PorterStemmer
 
-workbook = px.load_workbook('training-Obama-Romney-tweets.xlsx')
-sheet = workbook.get_sheet_by_name(name='Romney')
-
-max_row = sheet.max_row
-
-end = 'E' + str(max_row)
-csv_file = open('preprocessed.csv', 'w', newline='')
-wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+candidate1 = 'Obama'
+candidate2 = 'Romney'
+training_workbook = 'training-Obama-Romney-tweets.xlsx'
+testing_workbook = 'testing-Obama-Romney-tweets.xlsx'
+stemmer = PorterStemmer()
 
 stopWords = []
 with open('StopWords.txt') as stop_file:
@@ -18,9 +16,9 @@ with open('StopWords.txt') as stop_file:
 def preprocess(cur_tweet):
     cur_tweet = cur_tweet.lower().strip()
     cur_tweet = cur_tweet.replace("\"", "")
-    cur_tweet = re.sub(r'@[^\s]+', '', cur_tweet).strip()
+    cur_tweet = re.sub(r'(@[^\s]+)', 'ATUSER', cur_tweet).strip()
     cur_tweet = re.sub(r'#([^\s]+)', r'\1', cur_tweet).strip()
-    cur_tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', '', cur_tweet).strip()
+    cur_tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', 'URL', cur_tweet).strip()
     cur_tweet = re.sub(r'<[^>]+>', '', cur_tweet).strip()
     cur_tweet = re.sub(r'[\s]+', ' ', cur_tweet).strip()
     pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
@@ -30,20 +28,40 @@ def preprocess(cur_tweet):
     for word in words:
         val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", word)
         if word not in stopWords and val is not None and len(word) >= 2:
-            processed_tweet.append(word)
+            if word != 'ATUSER':
+                processed_tweet.append(stemmer.stem(word))
 
     final_tweet = " ".join(word for word in processed_tweet).strip()
     return final_tweet.strip()
 
+def workbookDef(workbookName, sheetName, outputFile):
+    workbook = px.load_workbook(workbookName)
+    sheet = workbook.get_sheet_by_name(name=sheetName)
+
+    max_row = sheet.max_row
+
+    end = 'E' + str(max_row)
+    csv_file = open(outputFile, 'w', newline='')
+    wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+
+    if(outputFile.endswith('training.csv')):
+        starting_cell = 'D3'
+    else :
+        starting_cell = 'A1'
+    for row in sheet[starting_cell:end]:
+        if row[1].value in [1, -1, 0]:
+            preprocessed_tweet = preprocess(str(row[0].value))
+            if preprocessed_tweet.strip():
+                tweets = [preprocessed_tweet, row[1].value]
+                wr.writerow(tweets)
+
+    csv_file.close()
 
 
-for row in sheet['D3':end]:
-    if row[1].value in [1,-1,0]:
-        preprocessed_tweet = preprocess(str(row[0].value))
-        if preprocessed_tweet.strip():
-            tweets = [preprocessed_tweet, row[1].value]
-            wr.writerow(tweets)
 
+workbookDef(training_workbook, candidate1, candidate1 + '_training.csv')
+workbookDef(testing_workbook, candidate1, candidate1 + '_testing.csv')
+workbookDef(training_workbook, candidate2, candidate2 + '_training.csv')
+workbookDef(testing_workbook, candidate2, candidate2 + '_testing.csv')
 
-csv_file.close()
 
